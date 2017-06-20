dee9ca3762cf1739807bf4b55c18a6f8d71420ae
Robin van der Vleuten <robin@webstronauts.co>
2017-06-20 09:20:51 +0200

    Borrowed some test cases from redux (#1)

--- test/index.test.js
+++ test/index.test.js
@@ -1,35 +1,116 @@
 import flucon, { thunk } from '../src';
+import todos, { addTodo } from './helpers/todos';
 
-const increaseCounter = () => ({ dispatch }) => dispatch({ type: 'INCREMENT' });
+const unknownAction = () => ({ type: 'UNKNOWN' });
 
 describe('flucon', () => {
-  it('should work', () => {
-    const ACTIONS = {
-      INCREMENT: state => ({ counter: state.counter + 1 }),
-      DECREMENT: state => ({ counter: state.counter - 1 }),
-    };
+  it('applies the reducer to the previous state', () => {
+    const store = flucon(todos);
+    expect(store.getState()).toEqual([]);
 
-    const store = flucon(
-      (state = { counter: 0 }, action) =>
-        action && ACTIONS[action.type]
-          ? ACTIONS[action.type](state, action)
-          : state
-    );
+    store.dispatch(unknownAction());
+    expect(store.getState()).toEqual([]);
 
-    store(thunk());
+    store.dispatch(addTodo('Hello'));
+    expect(store.getState()).toEqual([
+      {
+        id: 1,
+        text: 'Hello',
+      },
+    ]);
 
-    const unsubscribe = store(() => (action, next) => {
-      console.log(action);
-      return next(action);
-    });
+    store.dispatch(addTodo('World'));
+    expect(store.getState()).toEqual([
+      {
+        id: 1,
+        text: 'Hello',
+      },
+      {
+        id: 2,
+        text: 'World',
+      },
+    ]);
+  });
 
-    store.dispatch(increaseCounter());
+  it('applies the reducer to the initial state', () => {
+    const store = flucon(todos, [
+      {
+        id: 1,
+        text: 'Hello',
+      },
+    ]);
+    expect(store.getState()).toEqual([
+      {
+        id: 1,
+        text: 'Hello',
+      },
+    ]);
 
-    unsubscribe();
+    store.dispatch(unknownAction());
+    expect(store.getState()).toEqual([
+      {
+        id: 1,
+        text: 'Hello',
+      },
+    ]);
 
-    store.dispatch(increaseCounter());
-    store.dispatch(increaseCounter());
+    store.dispatch(addTodo('World'));
+    expect(store.getState()).toEqual([
+      {
+        id: 1,
+        text: 'Hello',
+      },
+      {
+        id: 2,
+        text: 'World',
+      },
+    ]);
+  });
 
-    console.log(store.getState());
+  it('supports multiple subscriptions', () => {
+    const store = flucon(todos);
+    const listenerA = jest.fn((action, next) => next(action));
+    const listenerB = jest.fn((action, next) => next(action));
+
+    let unsubscribeA = store(() => listenerA);
+    store.dispatch(unknownAction());
+    expect(listenerA.mock.calls.length).toBe(1);
+    expect(listenerB.mock.calls.length).toBe(0);
+
+    store.dispatch(unknownAction());
+    expect(listenerA.mock.calls.length).toBe(2);
+    expect(listenerB.mock.calls.length).toBe(0);
+
+    const unsubscribeB = store(() => listenerB);
+    expect(listenerA.mock.calls.length).toBe(2);
+    expect(listenerB.mock.calls.length).toBe(0);
+
+    store.dispatch(unknownAction());
+    expect(listenerA.mock.calls.length).toBe(3);
+    expect(listenerB.mock.calls.length).toBe(1);
+
+    unsubscribeA();
+    expect(listenerA.mock.calls.length).toBe(3);
+    expect(listenerB.mock.calls.length).toBe(1);
+
+    store.dispatch(unknownAction());
+    expect(listenerA.mock.calls.length).toBe(3);
+    expect(listenerB.mock.calls.length).toBe(2);
+
+    unsubscribeB();
+    expect(listenerA.mock.calls.length).toBe(3);
+    expect(listenerB.mock.calls.length).toBe(2);
+
+    store.dispatch(unknownAction());
+    expect(listenerA.mock.calls.length).toBe(3);
+    expect(listenerB.mock.calls.length).toBe(2);
+
+    unsubscribeA = store(() => listenerA);
+    expect(listenerA.mock.calls.length).toBe(3);
+    expect(listenerB.mock.calls.length).toBe(2);
+
+    store.dispatch(unknownAction());
+    expect(listenerA.mock.calls.length).toBe(4);
+    expect(listenerB.mock.calls.length).toBe(2);
   });
 });
